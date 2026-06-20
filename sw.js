18d19c97754020671078cf6eb930eda1591be665
Jonathanventu10-png <jonathan.ventu10@gmail.com>
2026-06-20 15:35:44 -0600

    Update sw.js for Recovery 7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='goalapp-recovery6-v1';
+const CACHE_NAME='goalapp-recovery7-v1';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('goalapp-')&&k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
